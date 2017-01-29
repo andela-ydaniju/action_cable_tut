0178a6903ae2339944f6caef585e446ac12649c7
andela-ydaniju <yusuf.daniju@andela.com>
2017-01-29 22:10:00 +0100

    add username to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,9 +39,9 @@ before_action :configure_sign_up_params, only: [:create]
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- db/migrate/20170129062840_devise_create_users.rb
+++ db/migrate/20170129062840_devise_create_users.rb
@@ -3,6 +3,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :username, null: false, default: ""
+
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -36,6 +38,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username, unique: true
+    
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
