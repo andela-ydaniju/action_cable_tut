86c6fe7b4b1500aec88bf789d7e194df9d3064e7
andela-ydaniju <yusuf.daniju@andela.com>
2017-01-29 08:44:26 +0100

    user authentication using devise

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < Devise::SessionsController
+before_action :configure_sign_in_params, only: [:create]
+before_action :login_required, only: [:destroy]
+
+  # GET /resource/sign_in
+  def new
+    super
+  end
+
+  # POST /resource/sign_in
+  def create
+    super
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    super
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users', path_names: { sign_in: 'login',
+    sign_out: 'logout', password: 'secret', confirmation: 'verification'
+  }, controllers: { registrations: 'users', sessions: 'sessions' }
+
   root to: 'home#index'
 end
